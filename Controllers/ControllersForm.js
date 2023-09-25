6e22f279062ba119c1f024c52b8aac02be39706c
Ignacio <ignaciosabiajes@gmail.com>
2023-09-25 11:41:22 -0300

    backend stablishied with insertUser

--- Controllers/ControllersForm.js
+++ Controllers/ControllersForm.js
@@ -1,2 +1,19 @@
 const { defaults } = require("pg");
 const knex = require("../Config/knexfile");
+
+
+exports.registerUser = async (req,res)=>{
+    try {
+        const user = {
+            name : req.body.name,
+            mail: req.body.mail,
+            phone: req.body.phone,
+            plan : req.body.plan,
+            addons : req.body.addons
+        }
+        
+    } catch (error) {
+        
+    }
+
+}
